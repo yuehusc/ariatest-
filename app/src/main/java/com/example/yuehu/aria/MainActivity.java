d880fe52d2e61ba31b4352f106daad79ed671db4
Loaner <yue.hu@snapchat.com>
2017-05-08 10:30:36 -0700

    trying payment

--- app/src/main/java/com/example/yuehu/aria/MainActivity.java
+++ app/src/main/java/com/example/yuehu/aria/MainActivity.java
@@ -4,12 +4,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.yuehu.aria.util.IabHelper;
+import com.example.yuehu.aria.util.IabResult;
+import com.example.yuehu.aria.util.Purchase;
+
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "PaymentService";
+
+    private String base64EncodedPublicKey = "CONSTRUCT_YOUR_KEY_AND_PLACE_IT_HERE";
+
+    static final String SKU_PREMIUM = "premium";
+    static final String SKU_GAS = "gas";
+
+    // SKU for our subscription (infinite gas)
+    static final String SKU_INFINITE_GAS_MONTHLY = "infinite_gas_monthly";
+    static final String SKU_INFINITE_GAS_YEARLY = "infinite_gas_yearly";
+
+    // (arbitrary) request code for the purchase flow
+    static final int RC_REQUEST = 10001;
+
+    private IabHelper mPaymentService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +54,23 @@ public class MainActivity extends AppCompatActivity {
                 directToSnapchat(link);
             }
         });
+
+        Button confirmPayment = (Button) findViewById(R.id.confirm_button);
+        confirmPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pay();
+            }
+        });
+
+        mPaymentService = new IabHelper(getApplicationContext(), base64EncodedPublicKey);
+        mPaymentService.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            @Override
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "onIabSetupFinished:success=" + result.isSuccess());
+            }
+        });
+        mPaymentService.enableDebugLogging(true);
     }
 
     private void directToSnapchat(String url) {
@@ -49,5 +87,142 @@ public class MainActivity extends AppCompatActivity {
             i.setData(uri);
             startActivity(i);
         }
+    }
+
+    private void pay() {
+        //mPaymentService.c
+
+        /* TODO: for security, generate your payload here for verification. See the comments on
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use
+         *        an empty string, but on a production app you should carefully generate this. */
+        String payload = "";
+
+        try {
+            mPaymentService.launchPurchaseFlow(this, SKU_GAS, RC_REQUEST,
+                    mPurchaseFinishedListener, payload);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            Log.d(TAG, "Error launching purchase flow. Another async operation in progress.");
+        }
+    }
+
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+
+            // if we were disposed of in the meantime, quit.
+            if (mPaymentService == null) return;
+
+            if (result.isFailure()) {
+                Log.d(TAG, "Error purchasing: " + result);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                Log.d(TAG, "Error purchasing. Authenticity verification failed.");
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+            if (purchase.getSku().equals(SKU_GAS)) {
+                // bought 1/4 tank of gas. So consume it.
+                Log.d(TAG, "Purchase is gas. Starting gas consumption.");
+                try {
+                    mPaymentService.consumeAsync(purchase, mConsumeFinishedListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    complain("Error consuming gas. Another async operation in progress.");
+                    return;
+                }
+            }
+            else if (purchase.getSku().equals(SKU_PREMIUM)) {
+                // bought the premium upgrade!
+                Log.d(TAG, "Purchase is premium upgrade. Congratulating user.");
+                alert("Thank you for upgrading to premium!");
+//                mIsPremium = true;
+//                updateUi();
+//                setWaitScreen(false);
+            }
+            else if (purchase.getSku().equals(SKU_INFINITE_GAS_MONTHLY)
+                    || purchase.getSku().equals(SKU_INFINITE_GAS_YEARLY)) {
+                // bought the infinite gas subscription
+                Log.d(TAG, "Infinite gas subscription purchased.");
+                alert("Thank you for subscribing to infinite gas!");
+//                mSubscribedToInfiniteGas = true;
+//                mAutoRenewEnabled = purchase.isAutoRenewing();
+//                mInfiniteGasSku = purchase.getSku();
+//                mTank = TANK_MAX;
+//                updateUi();
+//                setWaitScreen(false);
+            }
+        }
+    };
+
+    // Called when consumption is complete
+    IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+            Log.d(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
+
+            // if we were disposed of in the meantime, quit.
+            if (mPaymentService == null) return;
+
+            // We know this is the "gas" sku because it's the only one we consume,
+            // so we don't check which sku was consumed. If you have more than one
+            // sku, you probably should check...
+            if (result.isSuccess()) {
+                // successfully consumed, so we apply the effects of the item in our
+                // game world's logic, which in our case means filling the gas tank a bit
+                Log.d(TAG, "Consumption successful. Provisioning.");
+                //mTank = mTank == TANK_MAX ? TANK_MAX : mTank + 1;
+                //saveData();
+            }
+            else {
+                complain("Error while consuming: " + result);
+            }
+            //updateUi();
+            Log.d(TAG, "End consumption flow.");
+        }
+    };
+
+    /** Verifies the developer payload of a purchase. */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         *
+         * WARNING: Locally generating a random string when starting a purchase and
+         * verifying it here might seem like a good approach, but this will fail in the
+         * case where the user purchases an item on one device and then uses your app on
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         *
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         *
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on
+         *    one device work on other devices owned by the user).
+         *
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+
+        return true;
+    }
+
+    void complain(String message) {
+        Log.e(TAG, "**** TrivialDrive Error: " + message);
+        alert("Error: " + message);
+    }
+
+    void alert(String message) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(this);
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", null);
+        Log.d(TAG, "Showing alert dialog: " + message);
+        bld.create().show();
     }
 }
